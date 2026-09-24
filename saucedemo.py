ed541cf8d1197e56c99bcb2284102e30cec3c222
Sujan rijal <rizalsuzan2@gmail.com>
2026-09-24 14:03:47 +0545

    Comments

--- saucedemo.py
+++ saucedemo.py
@@ -1,21 +1,26 @@
+
+# Importing webdriver from selenium
 from selenium import webdriver
+# Importing time, helps to slow down every steps 
 import time 
+# Importing for finding web elements inside the website
 from selenium.webdriver.common.by import By
 
-driver = "webdriver"
+# Storing webdriver inside a variable and using to opean any browser 
 driver = webdriver.Chrome()
-time.sleep(2)
-driver.maximize_window()
-url = "https://www.saucedemo.com/"
-driver.get(url)
+time.sleep(2)   # pauses the program for a specified number of seconds.
+driver.maximize_window()   # Maximizing the browser
+url = "https://www.saucedemo.com/"  # Storing URL into a variable
+driver.get(url)     # Using the URL inside the browser
 time.sleep(2)
 
-#Locators
+# Locators -- important 
+# Among all the selenium locators we can use any
 username = driver.find_element (By.ID, "user-name")
 password = driver.find_element (By.ID, "password")
 login_button = driver.find_element (By.ID, "login-button")
 
-#Actions
+# After locating finally we give Actions to each feild  
 
 username.send_keys("standard_user")
 time.sleep(2)
